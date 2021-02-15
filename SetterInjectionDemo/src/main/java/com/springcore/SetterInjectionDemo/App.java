9eeb0d361f0491f1c5a91e64d3a4d65242b75fed
priyks <priyankakulkarni3431@gmail.com>
2021-02-15 23:59:30 +0530

    P schema added in config.xml file

--- SetterInjectionDemo/src/main/java/com/springcore/SetterInjectionDemo/App.java
+++ SetterInjectionDemo/src/main/java/com/springcore/SetterInjectionDemo/App.java
@@ -14,6 +14,10 @@ public class App
     {
     	 ApplicationContext context= new  ClassPathXmlApplicationContext("Config.xml");
          Student student1= (Student) context.getBean("Student1");
+         Student student2= (Student) context.getBean("Student2");
+         Student student3= (Student) context.getBean("Student3");
          System.out.println(student1);
+         System.out.println(student2);
+         System.out.println(student3);
     }
 }
